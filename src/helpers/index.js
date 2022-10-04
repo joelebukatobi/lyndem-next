343fc0642ffe7960b8a0c3dec421c81d9dec9ff2
Joel Onwuanaku <joelebuka@gmail.com>
2022-10-04 10:50:19 +0100

    Changes to .env

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,9 +3,3 @@ import cookie from 'cookie';
 export function parseCookies(req) {
   return cookie.parse(req ? req.headers.cookie || '' : '');
 }
-
-// NEXT_PUBLIC_API_URL = http://localhost:8000
-// NEXT_PUBLIC_WEB_URL = http://localhost:3000
-
-// NEXT_PUBLIC_API_URL = http://localhost:5000
-// NEXT_PUBLIC_WEB_URL = http://localhost:3000
