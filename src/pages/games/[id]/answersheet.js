1872ad9b78781f3865b91811d40256c8b1e0e23e
Joel Onwuanaku <joelebukatobi@pop-os.localdomain>
2023-04-09 12:06:06 +0100

    Fixed SVG issue for answer sheet on WebKIT browsers

--- src/pages/games/[id]/answersheet.js
+++ src/pages/games/[id]/answersheet.js
@@ -55,13 +55,8 @@ export default function about() {
     <>
       <Navbar />
       <Container>
-        <div className="h-[92vh] flex flex-col w-full justify-center lg:px-[16rem] ">
-          <div className="absolute left-0 top-[10.4rem] z-[-1000]  w-[100%] h-[100%]">
-            <svg className="hidden md:block w-[100%] h-[100%] opacity-[.1]" preserveAspectRatio="none">
-              <use href="/assets/svg/sprite.svg#answer-sheet-bg" />
-            </svg>
-          </div>
-          <div className="flex flex-col items-center justify-center mt-[-16rem] mb-[2.4rem]">
+        <div className=" flex flex-col w-full justify-center lg:px-[16rem] relative mb-[4rem] pt-[16rem] pb-[24rem]">
+          <div className="flex flex-col items-center justify-center mb-[2.4rem]">
             <h3 className="font-BenchNine">Answer Search</h3>
             <hr className="w-[15%] bg-[#ff6300]" />
             <p className="font-Nunito text-center w-[75%] md:w-full">
@@ -69,10 +64,7 @@ export default function about() {
             </p>
             <p className="font-Nunito text-red-600">{error}</p>
           </div>
-          <form
-            onSubmit={handleSubmit}
-            className="w-full h-[8rem] flex items-center justify-center relative mb-[1.6rem]"
-          >
+          <form onSubmit={handleSubmit} className="w-full h-[8rem] flex items-center justify-center relative">
             <Input
               type={'text'}
               value={term}
@@ -91,21 +83,24 @@ export default function about() {
                 <path d="M20.9999 21L16.6499 16.65" stroke="" strokeWidth="2" strokeLinejoin="round" />
               </svg>
             </Button>
+
+            <div className="h-[32rem] w-full bg-white absolute top-[9.6rem] overflow-y-scroll">
+              {questions &&
+                questions.data.map((data) => (
+                  <div className="p-[1.6rem] border-[.16rem] shadow-[0_1px_2px_rgba(0,0,0,0.25)] gap-x-[1.6rem] mb-[1.6rem]">
+                    <div>
+                      <input
+                        type="button"
+                        onClick={(e) => getQuestion(data._id)}
+                        placeholder={data.question}
+                        className="placeholder:text-black text-black cursor-pointer"
+                        value={data.question}
+                      />
+                    </div>
+                  </div>
+                ))}
+            </div>
           </form>
-          {questions &&
-            questions.data.map((data) => (
-              <div className="p-[1.6rem] shadow-[0_2px_4px_rgba(0,0,0,0.25)] gap-x-[1.6rem] mb-[1.6rem]">
-                <div>
-                  <input
-                    type="button"
-                    onClick={(e) => getQuestion(data._id)}
-                    placeholder={data.question}
-                    className="placeholder:text-black text-black cursor-pointer"
-                    value={data.question}
-                  />
-                </div>
-              </div>
-            ))}
         </div>
         {modal && (
           <div
@@ -127,7 +122,7 @@ export default function about() {
                 <div className="flex gap-x-[1.6rem] items-center w-2/4">
                   <p className="uppercase font-Domine font-bold text-[#458AD7] w-[10%]">a</p>
                   <div className="relative flex items-center justify-center w-[90%]">
-                    <svg className="" viewBox="0 0 3296 956">
+                    <svg className="w-full" viewBox="0 0 3296 956">
                       <path
                         d="M318.812 1.77482L4.16751 471.775C2.38829 474.432 4.29321 478 7.49149 478H3288.4C3291.62 478 3293.52 474.391 3291.7 471.737L2969.19 1.73681C2968.45 0.649698 2967.21 0 2965.89 0H322.136C320.801 0 319.554 0.665729 318.812 1.77482Z"
                         fill="#225D9F"
@@ -142,7 +137,7 @@ export default function about() {
                 <div className="flex gap-x-[1.6rem] items-center w-2/4">
                   <p className="uppercase font-Domine font-bold text-[#458AD7] w-[10%]">c</p>
                   <div className="relative flex items-center justify-center w-[90%]">
-                    <svg className="" viewBox="0 0 3296 956">
+                    <svg className="w-full" viewBox="0 0 3296 956">
                       <path
                         d="M318.812 1.77482L4.16751 471.775C2.38829 474.432 4.29321 478 7.49149 478H3288.4C3291.62 478 3293.52 474.391 3291.7 471.737L2969.19 1.73681C2968.45 0.649698 2967.21 0 2965.89 0H322.136C320.801 0 319.554 0.665729 318.812 1.77482Z"
                         fill="#225D9F"
@@ -159,7 +154,7 @@ export default function about() {
                 <div className="flex gap-x-[1.6rem] items-center w-2/4">
                   <p className="uppercase font-Domine font-bold text-[#458AD7] w-[10%]">b</p>
                   <div className="relative flex items-center justify-center w-[90%]">
-                    <svg className="" viewBox="0 0 3296 956">
+                    <svg className="w-full" viewBox="0 0 3296 956">
                       <path
                         d="M318.812 1.77482L4.16751 471.775C2.38829 474.432 4.29321 478 7.49149 478H3288.4C3291.62 478 3293.52 474.391 3291.7 471.737L2969.19 1.73681C2968.45 0.649698 2967.21 0 2965.89 0H322.136C320.801 0 319.554 0.665729 318.812 1.77482Z"
                         fill="#225D9F"
@@ -174,7 +169,7 @@ export default function about() {
                 <div className="flex gap-x-[1.6rem] items-center w-2/4">
                   <p className="uppercase font-Domine font-bold text-[#458AD7] w-[10%]">d</p>
                   <div className="relative flex items-center justify-center w-[90%]">
-                    <svg className="" viewBox="0 0 3296 956">
+                    <svg className="w-full" viewBox="0 0 3296 956">
                       <path
                         d="M318.812 1.77482L4.16751 471.775C2.38829 474.432 4.29321 478 7.49149 478H3288.4C3291.62 478 3293.52 474.391 3291.7 471.737L2969.19 1.73681C2968.45 0.649698 2967.21 0 2965.89 0H322.136C320.801 0 319.554 0.665729 318.812 1.77482Z"
                         fill="#225D9F"
